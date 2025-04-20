ab60d8da2d59274df9f35f412fd2792e41e9bae6
Mauboa <mba097@gmail.com>
2025-04-20 02:34:32 -0300

    fix: remove unused data

--- src/whatsapp/dto/send-whatsapp.dto.ts
+++
@@ -1,8 +0,0 @@
-// src/whatsapp/dto/send-whatsapp.dto.ts
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class SendWhatsappDto {
-  @IsNotEmpty()
-  @IsString()
-  message: string;
-}
--- src/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp.controller.ts
@@ -1,7 +1,6 @@
 // src/whatsapp/whatsapp.controller.ts
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { WhatsappService } from './whatsapp.service';
-import { SendWhatsappDto } from './dto/send-whatsapp.dto';
 
 @Controller('whatsapp')
 export class WhatsappController {
